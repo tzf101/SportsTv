ccb3c38536de20ec1c5cd54209995223394bd8c1
Tariq <58070824+Tariquzzaman-faisal@users.noreply.github.com>
2023-04-20 11:18:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Tv = [
     { 'name': 'Toffee',             'type': 'Both',     'lang': 'English',  'link': 'https://toffeelive.com/live-tv/all' },
-    { 'name': 'Rabbitholebd',       'type': 'Both',     'lang': 'English',  'link': 'https://www.rabbitholebd.com/live' },
+    { 'name': 'Rabbitholebd Region unlocked',       'type': 'Both',     'lang': 'English',  'link': 'https://yewtu.be/channel/UCNU0Vv6U1xZS82cIQrgDeqw' },
     { 'name': 'Totalsportek',       'type': 'Football', 'lang': 'English',  'link': 'https://totalsportek.pro/soccer/' },
     { 'name': 'Vip Stand',          'type': 'Football', 'lang': 'English',  'link': 'https://www.vipstand.se/live-sports-stream' },
     { 'name': 'Cricstream',         'type': 'Cricket',  'lang': 'English',  'link': 'https://watch.cricstream.me/' },
+    { 'name': 'Crichdtv',           'type': 'Cricket',  'lang': 'English',  'link': 'https://crichd.tv/' },
     { 'name': 'Beinmatch',          'type': 'Football', 'lang': 'Arabic',   'link': 'https://beinmatch.one/' },
     { 'name': 'Yallashoot',         'type': 'Football', 'lang': 'Arabic',   'link': 'https://stad.yalla-shoot.io/6dec/' },
     { 'name': 'Facebook Stream',    'type': 'Football', 'lang': 'Any',      'link': 'https://www.facebook.com/search/videos?q=football%20live&filters=eyJ2aWRlb3NfbGl2ZTowIjoie1wibmFtZVwiOlwidmlkZW9zX2xpdmVcIixcImFyZ3NcIjpcIlwifSIsInJwX2NyZWF0aW9uX3RpbWU6MCI6IntcIm5hbWVcIjpcImNyZWF0aW9uX3RpbWVcIixcImFyZ3NcIjpcIntcXFwic3RhcnRfeWVhclxcXCI6XFxcIjIwMjJcXFwiLFxcXCJzdGFydF9tb250aFxcXCI6XFxcIjIwMjItMTJcXFwiLFxcXCJlbmRfeWVhclxcXCI6XFxcIjIwMjJcXFwiLFxcXCJlbmRfbW9udGhcXFwiOlxcXCIyMDIyLTEyXFxcIixcXFwic3RhcnRfZGF5XFxcIjpcXFwiMjAyMi0xMi0yMFxcXCIsXFxcImVuZF9kYXlcXFwiOlxcXCIyMDIyLTEyLTIwXFxcIn1cIn0ifQ%3D%3D' },
